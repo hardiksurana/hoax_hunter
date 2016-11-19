8b3652ce688bab861cff4aa11f151d401c2f6d81
hardiksurana <hardiksurana01@gmail.com>
2016-11-20 03:42:36 +0530

    final news detection classification code

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from bs4 import BeautifulSoup
 from google import search
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import feedparser
 from newspaper import Article
+from fuzzywuzzy import fuzz
+import getTrusted
 
-def url_check(trusted, hoax):
+def url_check(trusted, hoax, url):
     for i in range (0,len(trusted)):
         if trusted[i] in url:
-            print("authentic")
-            authenticity_flag = 1
+            # print("authentic")
+            authenticity_flag = 0
     for i in range(0,len(hoax)):
         hoax[i]=hoax[i].lower()
         if hoax[i] in url:
-            print("unauthentic")
-            authenticity_flag = 0
+            # print("unauthentic")
+            authenticity_flag = 1
         if(".com.co" or ".lo" in url):
-            authenticity_flag = 0
+            authenticity_flag = 1
     return authenticity_flag
 
 def sentiment_check(url, sid):
@@ -31,11 +34,11 @@ def sentiment_check(url, sid):
             no_quotes.append(text[i])
     no_quotes = " ".join(no_quotes)
     ss = sid.polarity_scores(no_quotes)
-    print("negative score is",ss['neg'])
-    if ss['neg']>0.5:
-        return 0
-    else:
+    # print("negative score is",ss['neg'])
+    if ss['neg'] > 0.5:
         return 1
+    else:
+        return 0
     # for k in sorted(ss):
     #     print('{0}: {1} \n'.format(k, ss[k]), end='')
 
@@ -58,18 +61,63 @@ hoax = list()
 for x in hoax_sites:
     hoax.append(x.text)
 
-x = input("\nPlease enter the text query: ")
 authenticity_flag = 2
-stop_count = 0
-score = 0
 
 # generated a score based on 2 tests
-for url in search(x, stop=1):
-    print(url)
+def calculate_hoax_score(url):
+    stop_count = 0
+    # for url in search(query, stop=1):
+    hoax_score = 0
     if(stop_count > 10):
         break
     stop_count += 1
-    score += url_check(trusted, hoax)
-    score += sentiment_check(url, sid)
-    print("Score is ",score)
-    score = 0
+
+    # print("Searched query: "+url)
+    # gets searched query's article's content
+    searched_url_content = Article(url, language='en')
+    searched_url_content.download()
+    searched_url_content.parse()
+    searched_content = ' '.join(searched_url_content.text[:].split('\n'))
+
+    # search for similar content from RSS feed sites
+    for url_rss in getTrusted.get_hoax_links():
+        # print("RSS url: "+url_rss)
+        rss_limit = 0
+        d = feedparser.parse(url_rss)
+        for news in d['entries']:
+            # TODO: replace with gensim or other method
+            rss_limit += 1
+            if(rss_limit > 10):
+                break
+            if(fuzz.token_set_ratio(query, news['title_detail']['value']) >80):
+                # print("title matches")
+                link = news['link']
+                paper = Article(link, language='en')
+                paper.download()
+                paper.parse()
+                text = ' '.join(paper.text[:].split('\n'))
+                # TODO: replace with gensim or other method for efficient comparison
+                if(fuzz.token_set_ratio(text, searched_content) > 80):
+                    # print(x," and the news is        ",news['title_detail']['value'])
+                    # print("this news is authentic")
+                    hoax_score += 0
+                else:
+                    # print(x," and the news is        ",news['title_detail']['value'])
+                    # print("this news is hoax")
+                    hoax_score += 1
+            else:
+                pass
+    hoax_score += url_check(trusted, hoax, url)
+    hoax_score += sentiment_check(url, sid)
+    # print("Score is ",hoax_score)
+    return hoax_score
+
+for url in search(query, stop=1):
+    hoax_score = calculate_hoax_score(url)
+    # TODO: render template with other article content and set the flag appropriately
+    content = Article(url, language='en')
+    content.download()
+    content.parse()
+    text_content = ' '.join(content.text[:].split('\n'))
+
+print(calculate_hoax_score("Trump and British Families"))
