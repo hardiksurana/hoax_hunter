ce8fbc689d8f0bc1fee1e23e14dbbc0c197f8305
hardiksurana <hardiksurana01@gmail.com>
2016-11-19 11:30:13 +0530

    getting news articles from RSS feeds

--- classification.py
+++ classification.py
@@ -1,1 +1,23 @@
-print("classification")
+# working on classification using naive bayes classifier
+
+# Steps:
+#     get news dataset-authentic and hoax using RSS feeds
+#     create csv file with the data
+#     train the model
+#     test the model
+
+# get RSS feeds
+import feedparser
+from newspaper import Article
+rss_url = 'http://www.business-standard.com/rss/beyond-business-104.rss'
+d = feedparser.parse(rss_url)
+# print(d['entries'])
+
+for news in d['entries']:
+    link = news['link']
+    paper = Article(link, language='en')
+    paper.download()
+    paper.parse()
+    # print(paper.title)
+    print(paper.text[:])
+    print('-'*100)
