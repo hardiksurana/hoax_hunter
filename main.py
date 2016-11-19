1f1048979803c0f2b7b24b8f26ff62a1da4d74e4
hardiksurana <hardiksurana01@gmail.com>
2016-11-19 22:46:30 +0530

    created test functions

--- main.py
+++ main.py
@@ -2,47 +2,67 @@ from bs4 import BeautifulSoup
 from google import search
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from newspaper import Article
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sid=SentimentIntensityAnalyzer()
-soup = BeautifulSoup(open('list.html'),'lxml')
-y=soup.find(id="lol");
-trusted=[]
-soup1 = BeautifulSoup(open('list_hoax.html'),'lxml')
-y=soup1.find_all('li')
-hoax=[]
-for x in y:
-    hoax.append(x.text)
-return hoax
-all=y.find_all('a')
-for  x in all:
-    x=x.get('href').split("/")
-    if(x[2] not in trusted):
-        trusted.append(x[2])
-# print(trusted)
-x=input("\n\nPlease enter the tezxt query: ")
-authenticity_flag=0;
-for url in search(x, stop=1):
+
+def url_check(trusted, hoax):
     for i in range (0,len(trusted)):
-        if url in trusted[i]:
-            authenticity_flag=1
+        if trusted[i] in url:
+            authenticity_flag = 1
     for i in range(0,len(hoax)):
-        if url in hoax[i]:
-            authenticity_flag=0
+        if hoax[i] in url:
+            authenticity_flag = 0
         if(".com.co/" or ".lo/" in url):
-            authenticity_flag=0
-    article=Article(url)
+            authenticity_flag = 0
+    return authenticity_flag
+
+def sentiment_check(url, sid):
+    article = Article(url)
     article.download()
     article.parse()
-    # print(article.text)
-    text=article.text
-    text=text.split('\u201c')
-    no_quotes=[]
+    text = article.text
+    text = text.split('\u201c')
+    no_quotes = []
     for i in range(0,len(text)):
         if '\u201d' in text[i]:
             no_quotes.append(text[i].split('\u201d')[1])
         else:
             no_quotes.append(text[i])
-    no_quotes=" ".join(no_quotes)
+    no_quotes = " ".join(no_quotes)
     ss = sid.polarity_scores(no_quotes)
-    for k in sorted(ss):
-        print('{0}: {1}, '.format(k, ss[k]), end='')
+    if kk in sorted(ss) is 'neu':
+        return 1
+    else:
+        return 0
+    # for k in sorted(ss):
+    #     print('{0}: {1} \n'.format(k, ss[k]), end='')
+
+sid = SentimentIntensityAnalyzer()
+
+# gets authentic website links
+soup = BeautifulSoup(open('list.html'),'lxml')
+authentic_sites = soup.find(id="lol")
+trusted = list()
+temp = authentic_sites.find_all('a')
+for x in temp:
+    x = x.get('href').split("/")
+    if(x[2] not in trusted):
+        trusted.append(x[2])
+
+# gets hoax website links
+soup1 = BeautifulSoup(open('list_hoax.html'),'lxml')
+hoax_sites = soup1.find_all('li')
+hoax = list()
+for x in hoax_sites:
+    hoax.append(x.text)
+
+x = input("\nPlease enter the text query: ")
+authenticity_flag = 2
+stop_count = 0
+score = 0
+
+# generated a score based on 2 tests
+for url in search(x, stop=1):
+    if(stop_count > 10):
+        break
+    stop_count += 1
+    score += url_check(trusted, hoax)
+    score += sentiment_check(url, sid)
